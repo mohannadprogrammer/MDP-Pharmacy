0781465fa4a271fb64fc3b0d155fca9599f86c44
Abubaker Motasem <Abubaker@mazin.biz>
2019-07-31 03:48:24 -0700

    solve conflict in package.json

--- src/router.js
+++ src/router.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react"
-import { Route, Switch } from "react-router-dom"
-import Form from "./components/ItemRejesterForm/form"
-import Printdata from './DB/DBcomponent'
-
-//it a moduals .
-function router() {
-    return (
-        <Switch>
-            <Route to="/" component={Printdata} />
-        </Switch>
-    )
-}
-export default router;
-=======
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./Views/Login";
@@ -27,20 +11,19 @@ import suppliers from "./Views/supplier"
 //it a moduals contant all routes in the app
 
 class router extends React.Component {
-  render() {
-    return (
-      <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/dashboard" exact component={Dashboard} />
-          
-          <Route path="/Sales" exact component={Sales} />
-          <Route path="/items" exact component={Items} />
-          <Route path="/stores" exact component={Stores} />
-          <Route path="/suppliers" exact component={suppliers} />
-          <Route path="/users" exact component={Users} />
-      </Switch>
-    );
-  }
+    render() {
+        return (
+            <Switch>
+                <Route path="/" exact component={Login} />
+                <Route path="/dashboard" exact component={Dashboard} />
+
+                <Route path="/Sales" exact component={Sales} />
+                <Route path="/items" exact component={Items} />
+                <Route path="/stores" exact component={Stores} />
+                <Route path="/suppliers" exact component={suppliers} />
+                <Route path="/users" exact component={Users} />
+            </Switch>
+        );
+    }
 }
 export default router;
->>>>>>> b74068a067a2365087557222b8bf25a90cc8cf04
